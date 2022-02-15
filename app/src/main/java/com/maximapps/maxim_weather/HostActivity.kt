e1c3dfd5bba2b0f8c50a183c648df2f1d953eb95
Maxim Smolyakov <maxmerklol@gmail.com>
2022-02-16 01:26:56 +0800

    Update host activity

--- app/src/main/java/com/maximapps/maxim_weather/HostActivity.kt
+++ app/src/main/java/com/maximapps/maxim_weather/HostActivity.kt
@@ -2,10 +2,22 @@ package com.maximapps.maxim_weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.maximapps.maxim_weather.databinding.ActivityHostBinding
 
 class HostActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_host)
+        setContentView(ActivityHostBinding.inflate(layoutInflater).root)
+        navController = getNavHostFragment().navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    private fun getNavHostFragment(): NavHostFragment {
+        return supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
     }
 }
