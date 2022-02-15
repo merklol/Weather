8d4195d6f0eb37bc117f55903aa767fdb44f3706
Maxim Smolyakov <maxmerklol@gmail.com>
2022-02-16 01:22:19 +0800

    Rename main activity

--- app/src/main/java/com/maximapps/maxim_weather/HostActivity.kt
+++ app/src/main/java/com/maximapps/maxim_weather/HostActivity.kt
@@ -3,9 +3,9 @@ package com.maximapps.maxim_weather
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_host)
     }
 }
