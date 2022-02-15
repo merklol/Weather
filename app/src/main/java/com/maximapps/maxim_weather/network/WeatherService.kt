8430443b60b1e241f23e15f501d256fce8767918
Maxim Smolyakov <maxmerklol@gmail.com>
2022-02-16 01:30:21 +0800

    Update a retrofit service

--- app/src/main/java/com/maximapps/maxim_weather/network/WeatherService.kt
+++ app/src/main/java/com/maximapps/maxim_weather/network/WeatherService.kt
@@ -5,6 +5,7 @@ import io.reactivex.rxjava3.core.Observable
 import retrofit2.http.GET
 
 interface WeatherService {
+    //Temp solution
     @GET("forecast?q=London&appid=40116f7c79b9c42fd92351dcaf9c70d6")
     fun getForecast(): Observable<Response>
 }
