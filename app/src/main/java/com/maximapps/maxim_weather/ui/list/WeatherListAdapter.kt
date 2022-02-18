6b1aeb4af949f66af87bd0e5f5769f46703b4108
Maxim Smolyakov <maxmerklol@gmail.com>
2022-02-18 21:20:21 +0800

    Update the weather list adapter

--- app/src/main/java/com/maximapps/maxim_weather/ui/list/WeatherListAdapter.kt
+++ app/src/main/java/com/maximapps/maxim_weather/ui/list/WeatherListAdapter.kt
@@ -1,18 +1,15 @@
 package com.maximapps.maxim_weather.ui.list
 
-import androidx.navigation.NavController
-import com.maximapps.maxim_weather.R
 import com.maximapps.maxim_weather.core.listAdapterOf
+import com.maximapps.maxim_weather.domain.models.Forecast
 import com.maximapps.maxim_weather.ui.list.viewholders.ListViewHolder
 import com.maximapps.maxim_weather.ui.list.viewholders.TodayViewHolder
 
-fun weatherListAdapter(navController: NavController) = listAdapterOf(
+fun weatherListAdapter(onItemClick: (forecast: Forecast) -> Unit) = listAdapterOf(
     init = { viewGroup, viewType ->
         when (viewType) {
             ViewTypes.TodayView -> TodayViewHolder(viewGroup)
-            else -> ListViewHolder(viewGroup) {
-                navController.navigate(R.id.action_mainFragment_to_detailsFragment)
-            }
+            else -> ListViewHolder(viewGroup, onItemClick)
         }
     }, itemViewType = { if (it == ViewTypes.TodayView) 0 else ViewTypes.GroupView }
 )
