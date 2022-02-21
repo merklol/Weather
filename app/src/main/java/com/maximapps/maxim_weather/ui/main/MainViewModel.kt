4a4fcc8dbb132e3d8a7fb337182f25e2078354f5
Maxim Smolyakov <maxmerklol@gmail.com>
2022-02-21 16:39:43 +0800

    Update getForecast of the main viewModel

--- app/src/main/java/com/maximapps/maxim_weather/ui/main/MainViewModel.kt
+++ app/src/main/java/com/maximapps/maxim_weather/ui/main/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.maximapps.maxim_weather.ui.main
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.maximapps.maxim_weather.R
 import com.maximapps.maxim_weather.domain.WeatherRepository
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
@@ -15,15 +16,17 @@ class MainViewModel @Inject constructor(
     private val _liveData = MutableLiveData<MainState>()
     val liveData = _liveData
 
-    fun getForecast() {
-        repository.getForecast("Bangkok")
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .doOnSubscribe { _liveData.value = MainState.Loading }
-            .subscribe(
-                { _liveData.value = MainState.Success(it) },
-                { _liveData.value = MainState.Fail("Something went wrong...") }
-            )
+    fun getForecast(cityName: String) {
+        disposables.add(
+            repository.getForecast(cityName)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { _liveData.value = MainState.Loading }
+                .subscribe(
+                    { _liveData.value = MainState.Success(it) },
+                    { _liveData.value = MainState.Fail(R.string.error_message) }
+                )
+        )
     }
 
     override fun onCleared() {
