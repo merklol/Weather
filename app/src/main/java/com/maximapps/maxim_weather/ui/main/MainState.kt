0cb17434be2589e523276522dc3ddd278d40c68f
Maxim Smolyakov <maxmerklol@gmail.com>
2022-02-21 16:38:29 +0800

    Change the error message to string res id

--- app/src/main/java/com/maximapps/maxim_weather/ui/main/MainState.kt
+++ app/src/main/java/com/maximapps/maxim_weather/ui/main/MainState.kt
@@ -1,9 +1,10 @@
 package com.maximapps.maxim_weather.ui.main
 
-import com.maximapps.maxim_weather.domain.models.CityWeather
+import androidx.annotation.StringRes
+import com.maximapps.maxim_weather.domain.models.WeatherData
 
 sealed class MainState {
     object Loading : MainState()
-    data class Success(val response: CityWeather) : MainState()
-    data class Fail(val errorMessage: String) : MainState()
+    data class Success(val data: WeatherData) : MainState()
+    data class Fail(@StringRes val resId: Int) : MainState()
 }
