a4eb29976881faf9cb1af61734254755df0d38b5
Maxim Smolyakov <maxmerklol@gmail.com>
2022-02-21 16:35:34 +0800

    Format code style

--- app/src/main/java/com/maximapps/maxim_weather/HostActivity.kt
+++ app/src/main/java/com/maximapps/maxim_weather/HostActivity.kt
@@ -1,23 +1,5 @@
 package com.maximapps.maxim_weather
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import by.kirich1409.viewbindingdelegate.viewBinding
-import com.maximapps.maxim_weather.databinding.ActivityHostBinding
 
-class HostActivity : AppCompatActivity(R.layout.activity_host) {
-    private val binding by viewBinding(ActivityHostBinding::bind)
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setSupportActionBar(binding.toolbar)
-        binding.toolbar.setupWithNavController(getNavHostFragment().navController)
-    }
-
-    private fun getNavHostFragment(): NavHostFragment {
-        return supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-    }
-
-}
+class HostActivity : AppCompatActivity(R.layout.activity_host)
