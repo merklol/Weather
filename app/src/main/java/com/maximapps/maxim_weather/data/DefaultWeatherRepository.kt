d71d1eb6574cba52938757943fe1577de1117168
Maxim Smolyakov <maxmerklol@gmail.com>
2022-02-21 16:35:54 +0800

    Add KDoc comments

--- app/src/main/java/com/maximapps/maxim_weather/data/DefaultWeatherRepository.kt
+++ app/src/main/java/com/maximapps/maxim_weather/data/DefaultWeatherRepository.kt
@@ -1,19 +1,22 @@
 package com.maximapps.maxim_weather.data
 
-import com.maximapps.maxim_weather.data.mappers.ForecastMapper
+import com.maximapps.maxim_weather.data.mappers.ResponseMapper
 import com.maximapps.maxim_weather.domain.WeatherRepository
-import com.maximapps.maxim_weather.domain.models.CityWeather
+import com.maximapps.maxim_weather.domain.models.WeatherData
 import com.maximapps.maxim_weather.data.network.WeatherService
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
 
+/**
+ * Default implementation of the [WeatherRepository] interface.
+ */
 class DefaultWeatherRepository @Inject constructor(
     private val service: WeatherService,
-    private val mapper: ForecastMapper
+    private val mapper: ResponseMapper
 ) : WeatherRepository {
 
-    override fun getForecast(cityName: String): Single<CityWeather> {
+    override fun getForecast(cityName: String): Single<WeatherData> {
         return service.getForecast(cityName).subscribeOn(Schedulers.io()).map { mapper.map(it) }
     }
 }
