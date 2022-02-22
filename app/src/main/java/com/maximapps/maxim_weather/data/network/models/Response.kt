8d1e19f743b45674d87d1aa648a0d99f5a918411
Maxim Smolyakov <maxmerklol@gmail.com>
2022-02-22 16:21:59 +0800

    Move gropByDate method to the Response class

--- app/src/main/java/com/maximapps/maxim_weather/data/network/models/Response.kt
+++ app/src/main/java/com/maximapps/maxim_weather/data/network/models/Response.kt
@@ -1,6 +1,9 @@
 package com.maximapps.maxim_weather.data.network.models
 
 import com.google.gson.annotations.SerializedName
+import java.util.Calendar
+import java.util.Date
+import java.util.concurrent.TimeUnit
 
 data class Response(
     val cod: String,
@@ -8,4 +11,11 @@ data class Response(
     val city: CityEntity,
     @SerializedName("list")
     val forecastList: List<ForecastEntity>
-)
+) {
+    fun groupByDate() = forecastList.groupBy {
+        with(Calendar.getInstance()) {
+            time = Date(TimeUnit.SECONDS.toMillis(it.dt))
+            get(Calendar.DATE)
+        }
+    }
+}
