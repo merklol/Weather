4f68307d92a4ca0cb56d1bffe67b102db43212ae
Maxim Smolyakov <maxmerklol@gmail.com>
2022-02-22 16:18:28 +0800

    Change imports

--- app/src/main/java/com/maximapps/maxim_weather/data/DefaultWeatherRepository.kt
+++ app/src/main/java/com/maximapps/maxim_weather/data/DefaultWeatherRepository.kt
@@ -1,9 +1,9 @@
 package com.maximapps.maxim_weather.data
 
 import com.maximapps.maxim_weather.data.mappers.ResponseMapper
+import com.maximapps.maxim_weather.data.network.WeatherService
 import com.maximapps.maxim_weather.domain.WeatherRepository
 import com.maximapps.maxim_weather.domain.models.WeatherData
-import com.maximapps.maxim_weather.data.network.WeatherService
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 import javax.inject.Inject
