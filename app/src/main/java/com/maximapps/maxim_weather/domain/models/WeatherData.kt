538189c21ad9e95dceb1c81c79c1a7d7321afecf
Maxim Smolyakov <maxmerklol@gmail.com>
2022-02-22 16:24:50 +0800

    Rollback to the previous option

--- app/src/main/java/com/maximapps/maxim_weather/domain/models/WeatherData.kt
+++ app/src/main/java/com/maximapps/maxim_weather/domain/models/WeatherData.kt
@@ -1,10 +1,4 @@
 package com.maximapps.maxim_weather.domain.models
 
-sealed class ResponseResult {
-    data class Success(
-        val cityName: String,
-        val detailedForecast: List<DetailedForecast>
-    ) : ResponseResult()
+data class WeatherData(val cityName: String, val detailedForecast: List<DetailedForecast>)
 
-    object Error : ResponseResult()
-}
