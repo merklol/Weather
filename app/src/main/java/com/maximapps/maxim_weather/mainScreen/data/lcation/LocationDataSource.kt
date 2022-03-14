768f993b2a7a68a8a864bb72a44319faad4b79de
Maxim Smolyakov <maxmerklol@gmail.com>
2022-03-14 22:46:58 +0800

    Refactor getLocation

--- app/src/main/java/com/maximapps/maxim_weather/mainScreen/data/lcation/LocationDataSource.kt
+++ app/src/main/java/com/maximapps/maxim_weather/mainScreen/data/lcation/LocationDataSource.kt
@@ -1,12 +1,11 @@
 package com.maximapps.maxim_weather.mainScreen.data.lcation
 
 import android.annotation.SuppressLint
-import android.location.Location
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.tasks.CancellationTokenSource
 import com.maximapps.maxim_weather.common.utils.asSingle
-import com.maximapps.maxim_weather.common.utils.toHours
+import com.maximapps.maxim_weather.common.utils.millisecondsToHours
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
@@ -14,14 +13,14 @@ import javax.inject.Inject
 class LocationDataSource @Inject constructor(private val client: FusedLocationProviderClient) {
     private val token = CancellationTokenSource().token
 
-    fun getLocation(): Single<Location> =
-        client.lastLocation.asSingle().flatMap { location: Location? ->
-            location?.let {
-                if ((System.currentTimeMillis() - it.time).toHours() > 1) {
-                    client.getCurrentLocation(PRIORITY_HIGH_ACCURACY, token).asSingle()
-                } else {
-                    Single.just(it)
-                }
-            } ?: client.getCurrentLocation(PRIORITY_HIGH_ACCURACY, token).asSingle()
-        }
+    fun getLocation(): Single<Coordinates> = with(client) {
+        lastLocation.asSingle(CancellationTokenSource()).filter {
+            (System.currentTimeMillis() - it.time).millisecondsToHours() <= 1
+        }.switchIfEmpty(
+            getCurrentLocation(PRIORITY_HIGH_ACCURACY, token).asSingle(
+                CancellationTokenSource()
+            )
+        ).map { Coordinates(it.latitude, it.longitude) }
+    }
 }
+
