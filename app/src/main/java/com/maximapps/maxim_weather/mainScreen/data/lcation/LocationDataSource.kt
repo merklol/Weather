437b7a1ebe83e5900f356a6b70e65d96ea8c16ee
Maxim Smolyakov <maxmerklol@gmail.com>
2022-03-15 00:36:28 +0800

    Refactor LocationDataSource

--- app/src/main/java/com/maximapps/maxim_weather/mainScreen/data/lcation/LocationDataSource.kt
+++ app/src/main/java/com/maximapps/maxim_weather/mainScreen/data/lcation/LocationDataSource.kt
@@ -1,6 +1,7 @@
 package com.maximapps.maxim_weather.mainScreen.data.lcation
 
 import android.annotation.SuppressLint
+import android.location.Location
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.tasks.CancellationTokenSource
@@ -11,16 +12,15 @@ import javax.inject.Inject
 
 @SuppressLint("MissingPermission")
 class LocationDataSource @Inject constructor(private val client: FusedLocationProviderClient) {
-    private val token = CancellationTokenSource().token
+    private val cancellationTokenSource = CancellationTokenSource()
 
-    fun getLocation(): Single<Coordinates> = with(client) {
-        lastLocation.asSingle(CancellationTokenSource()).filter {
-            (System.currentTimeMillis() - it.time).millisecondsToHours() <= 1
-        }.switchIfEmpty(
-            getCurrentLocation(PRIORITY_HIGH_ACCURACY, token).asSingle(
-                CancellationTokenSource()
-            )
-        ).map { Coordinates(it.latitude, it.longitude) }
-    }
+    fun getLocation(): Single<Coordinates> = client.lastLocation
+        .asSingle(cancellationTokenSource)
+        .filter { (System.currentTimeMillis() - it.time).millisecondsToHours() <= 1 }
+        .switchIfEmpty(getCurrentLocation())
+        .map { Coordinates(it.latitude, it.longitude) }
+
+    private fun getCurrentLocation() = client
+        .getCurrentLocation(PRIORITY_HIGH_ACCURACY, cancellationTokenSource.token)
+        .asSingle(cancellationTokenSource)
 }
-
