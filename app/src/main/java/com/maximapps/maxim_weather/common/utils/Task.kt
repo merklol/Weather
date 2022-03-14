fce1175f0281a6a37108d084758b672a5faa170b
Maxim Smolyakov <maxmerklol@gmail.com>
2022-03-14 22:50:35 +0800

    Add cancellable fo the emiiter

--- app/src/main/java/com/maximapps/maxim_weather/common/utils/Task.kt
+++ app/src/main/java/com/maximapps/maxim_weather/common/utils/Task.kt
@@ -1,12 +1,17 @@
 package com.maximapps.maxim_weather.common.utils
 
+import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.gms.tasks.Task
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.functions.Cancellable
+import java.lang.IllegalStateException
 
 /**
  * Represents Task as a Single.
  */
-fun <T : Any> Task<T>.asSingle(): Single<T> = Single.create { emitter ->
-    addOnSuccessListener(emitter::onSuccess)
-    addOnFailureListener(emitter::tryOnError)
-}
+fun <T : Any> Task<T>.asSingle(tokenSource: CancellationTokenSource? = null): Single<T> =
+    Single.create { emitter ->
+        addOnSuccessListener(emitter::onSuccess)
+        addOnFailureListener(emitter::tryOnError)
+        emitter.setCancellable { tokenSource?.cancel() }
+    }
