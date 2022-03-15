80073bbc1a61cff09490468961b035de822f1d8d
Maxim Smolyakov <maxmerklol@gmail.com>
2022-03-15 19:36:54 +0800

    Refactor MainFragment

--- app/src/main/java/com/maximapps/maxim_weather/mainScreen/ui/MainFragment.kt
+++ app/src/main/java/com/maximapps/maxim_weather/mainScreen/ui/MainFragment.kt
@@ -4,6 +4,7 @@ import android.Manifest.permission
 import android.content.Context
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.StringRes
 import androidx.core.view.isVisible
@@ -12,7 +13,6 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 import com.maximapps.maxim_weather.R
 import com.maximapps.maxim_weather.common.di.factory.ViewModelFactory
@@ -36,25 +36,12 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     private val adapter = ItemAdapter<GenericItem>()
     private val fastAdapter: GenericFastAdapter = FastAdapter.with(adapter)
 
-    private val rationaleDialog by lazy {
-        MaterialAlertDialogBuilder(requireContext())
-            .setTitle(R.string.rationale_dialog_title)
-            .setMessage(R.string.rationale_dialog_message)
-            .setPositiveButton(R.string.rationale_dialog_positive_btn_text) { dialog, _ ->
-                dialog.dismiss()
-            }
-            .create()
-    }
-
-    private val request = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-        viewModel.fetchNewForecastByLocation(it)
-    }
-
     override fun onAttach(context: Context) {
         super.onAttach(context)
         AndroidSupportInjection.inject(this)
     }
 
+    //region Initialization
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.fetchForecast("Shanghai")
@@ -68,12 +55,8 @@ class MainFragment : Fragment(R.layout.fragment_main) {
             rationaleDialogVisibility.observe(lifecycleScope) { showRationaleDialog() }
         }
         binding.toolbar.searchBtn.setOnClickListener(::showSearchDialog)
-        binding.toolbar.locationBtn.setOnClickListener(::requestPermission)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        rationaleDialog.dismiss()
+        val request = registerForActivityResult()
+        binding.toolbar.locationBtn.setOnClickListener { requestPermission(request) }
     }
 
     private fun showScreenTitle(title: String) {
@@ -90,26 +73,29 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         }
     }
 
-    private fun showErrorMessage(@StringRes resId: Int) {
+    private fun showErrorMessage(@StringRes resId: Int) =
         Snackbar.make(requireView(), resId, Snackbar.LENGTH_SHORT).show()
-    }
 
     private fun showProgressIndicator(isVisible: Boolean) {
         binding.weatherList.isVisible = !isVisible
         binding.progressIndicator.isVisible = isVisible
     }
 
-    private fun showRationaleDialog() {
-        rationaleDialog.show()
-    }
+    private fun showRationaleDialog() = findNavController()
+        .navigate(MainFragmentDirections.actionMainFragmentToRationaleDialog())
 
     @Suppress("unused_parameter")
-    private fun showSearchDialog(view: View) {
-        findNavController().navigate(MainFragmentDirections.actionMainFragmentToMainDialog())
-    }
+    private fun showSearchDialog(view: View) = findNavController()
+        .navigate(MainFragmentDirections.actionMainFragmentToMainDialog())
+
+    private fun registerForActivityResult() =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            viewModel.fetchNewForecastByLocation(isGranted)
+        }
 
     @Suppress("unused_parameter")
-    private fun requestPermission(view: View) {
-        request.launch(permission.ACCESS_FINE_LOCATION)
+    private fun requestPermission(activityResultLauncher: ActivityResultLauncher<String>) {
+        activityResultLauncher.launch(permission.ACCESS_FINE_LOCATION)
     }
+//endregion
 }
