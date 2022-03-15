299eaa9d989e3e2e68e37a4781a3b2d18b43accc
Maxim Smolyakov <maxmerklol@gmail.com>
2022-03-15 10:56:07 +0800

    Refactor asSingle extension function

--- app/src/main/java/com/maximapps/maxim_weather/common/utils/Task.kt
+++ app/src/main/java/com/maximapps/maxim_weather/common/utils/Task.kt
@@ -3,8 +3,6 @@ package com.maximapps.maxim_weather.common.utils
 import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.gms.tasks.Task
 import io.reactivex.rxjava3.core.Single
-import io.reactivex.rxjava3.functions.Cancellable
-import java.lang.IllegalStateException
 
 /**
  * Represents Task as a Single.
@@ -12,6 +10,6 @@ import java.lang.IllegalStateException
 fun <T : Any> Task<T>.asSingle(tokenSource: CancellationTokenSource? = null): Single<T> =
     Single.create { emitter ->
         addOnSuccessListener(emitter::onSuccess)
-        addOnFailureListener(emitter::tryOnError)
-        emitter.setCancellable { tokenSource?.cancel() }
+        addOnFailureListener(emitter::onError)
+        emitter.setCancellable { if (isCanceled) tokenSource?.cancel() }
     }
