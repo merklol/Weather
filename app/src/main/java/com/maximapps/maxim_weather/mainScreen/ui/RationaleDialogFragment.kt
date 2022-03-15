3a1cb680c00762e7669ee7adc568e6497cba46ed
Maxim Smolyakov <maxmerklol@gmail.com>
2022-03-15 19:35:58 +0800

    Add a RationaleDialog fragment

---
+++ app/src/main/java/com/maximapps/maxim_weather/mainScreen/ui/RationaleDialogFragment.kt
@@ -0,0 +1,17 @@
+package com.maximapps.maxim_weather.mainScreen.ui
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.maximapps.maxim_weather.R
+
+class RationaleDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(R.string.rationale_dialog_title)
+            .setMessage(R.string.rationale_dialog_message)
+            .setPositiveButton(R.string.rationale_dialog_positive_btn_text) { _, _ -> }
+            .create()
+}
