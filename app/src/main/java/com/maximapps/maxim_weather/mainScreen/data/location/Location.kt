0154f830119c97b1ad0908ab9c2f271cb5369ca6
Maxim Smolyakov <maxmerklol@gmail.com>
2022-03-16 18:29:57 +0800

    Change the return type of LocationDataSource

---
+++ app/src/main/java/com/maximapps/maxim_weather/mainScreen/data/location/Location.kt
@@ -0,0 +1,6 @@
+package com.maximapps.maxim_weather.mainScreen.data.location
+
+/**
+ * Device Location
+ */
+data class Location(val latitude: Double, val longitude: Double)
--- app/src/main/java/com/maximapps/maxim_weather/mainScreen/data/location/LocationDataSource.kt
+++ app/src/main/java/com/maximapps/maxim_weather/mainScreen/data/location/LocationDataSource.kt
@@ -1,4 +1,4 @@
-package com.maximapps.maxim_weather.mainScreen.data.lcation
+package com.maximapps.maxim_weather.mainScreen.data.location
 
 import androidx.annotation.RequiresPermission
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -6,7 +6,6 @@ import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.tasks.CancellationTokenSource
 import com.maximapps.maxim_weather.common.utils.asSingle
 import com.maximapps.maxim_weather.common.utils.millisecondsToHours
-import com.maximapps.maxim_weather.mainScreen.domain.models.Coordinates
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
@@ -14,11 +13,11 @@ class LocationDataSource @Inject constructor(private val client: FusedLocationPr
     private val cancellationTokenSource = CancellationTokenSource()
 
     @RequiresPermission("android.permission.ACCESS_FINE_LOCATION")
-    fun getLocation(): Single<Coordinates> = client.lastLocation
+    fun getLocation(): Single<Location> = client.lastLocation
         .asSingle(cancellationTokenSource)
         .filter { (System.currentTimeMillis() - it.time).millisecondsToHours() <= 1 }
         .switchIfEmpty(getCurrentLocation())
-        .map { Coordinates(it.latitude, it.longitude) }
+        .map { Location(it.latitude, it.longitude) }
 
     @RequiresPermission("android.permission.ACCESS_FINE_LOCATION")
     private fun getCurrentLocation() = client
