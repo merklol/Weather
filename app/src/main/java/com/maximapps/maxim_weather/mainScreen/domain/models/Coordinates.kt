4d8537bd1d47b936511687f53292fba6887229fa
Maxim Smolyakov <maxmerklol@gmail.com>
2022-03-16 18:30:25 +0800

    Move the Coordinates model to the domain layer

--- app/src/main/java/com/maximapps/maxim_weather/mainScreen/domain/models/Coordinates.kt
+++ app/src/main/java/com/maximapps/maxim_weather/mainScreen/domain/models/Coordinates.kt
@@ -1,3 +1,6 @@
-package com.maximapps.maxim_weather.mainScreen.data.lcation
+package com.maximapps.maxim_weather.mainScreen.domain.models
 
+/**
+ * Device geographic coordinates
+ */
 data class Coordinates(val latitude: Double, val longitude: Double)
