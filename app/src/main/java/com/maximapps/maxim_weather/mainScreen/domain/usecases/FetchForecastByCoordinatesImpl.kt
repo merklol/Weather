5a7b06c7c2703af5d201e0600f9414774256dfaa
Maxim Smolyakov <maxmerklol@gmail.com>
2022-03-17 21:29:16 +0800

    Rename the typealias

--- app/src/main/java/com/maximapps/maxim_weather/mainScreen/domain/usecases/FetchForecastByCoordinatesImpl.kt
+++ app/src/main/java/com/maximapps/maxim_weather/mainScreen/domain/usecases/FetchForecastByCoordinatesImpl.kt
@@ -8,12 +8,12 @@ import com.maximapps.maxim_weather.mainScreen.domain.models.WeatherData
 import io.reactivex.rxjava3.core.Single
 import javax.inject.Inject
 
-typealias FetchForecastByCoordinatesUseCase = SingleUseCase<Nothing, WeatherData>
+typealias FetchForecastByCoordinates = SingleUseCase<Nothing, WeatherData>
 
-class FetchForecastByCoordinates @Inject constructor(
+class FetchForecastByCoordinatesImpl @Inject constructor(
     private val locationRepository: LocationRepository,
     private val weatherRepository: WeatherRepository
-) : FetchForecastByCoordinatesUseCase {
+) : FetchForecastByCoordinates {
 
     @RequiresPermission("android.permission.ACCESS_FINE_LOCATION")
     override operator fun invoke(params: Nothing?): Single<WeatherData> = locationRepository
