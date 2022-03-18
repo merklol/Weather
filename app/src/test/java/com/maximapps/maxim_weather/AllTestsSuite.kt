973bb0a2d22f44b092be24a6dcb2532f70f23282
Maxim Smolyakov <maxmerklol@gmail.com>
2022-03-18 23:12:41 +0800

    Update the tests suite

--- app/src/test/java/com/maximapps/maxim_weather/AllTestsSuite.kt
+++ app/src/test/java/com/maximapps/maxim_weather/AllTestsSuite.kt
@@ -1,10 +1,15 @@
 package com.maximapps.maxim_weather
 
-import com.maximapps.maxim_weather.mainScreen.data.DefaultWeatherRepositoryTest
+import com.maximapps.maxim_weather.mainScreen.data.CityWeatherRepositoryImplTest
+import com.maximapps.maxim_weather.mainScreen.data.LocationWeatherRepositoryImplTest
 import com.maximapps.maxim_weather.mainScreen.ui.MainViewModelTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
-@Suite.SuiteClasses(DefaultWeatherRepositoryTest::class, MainViewModelTest::class)
+@Suite.SuiteClasses(
+    CityWeatherRepositoryImplTest::class,
+    LocationWeatherRepositoryImplTest::class,
+    MainViewModelTest::class
+)
 class AllTestsSuite
