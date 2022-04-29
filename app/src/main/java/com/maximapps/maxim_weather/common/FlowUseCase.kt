e86b579b377438ba9e31084e5976f187c2a6ce49
Maxim Smolyakov <maxmerklol@gmail.com>
2022-04-29 13:25:15 +0800

    Convert SingleUseCase to FlowUseCase

--- app/src/main/java/com/maximapps/maxim_weather/common/FlowUseCase.kt
+++ app/src/main/java/com/maximapps/maxim_weather/common/FlowUseCase.kt
@@ -1,11 +1,10 @@
 package com.maximapps.maxim_weather.common
 
-import io.reactivex.rxjava3.core.Single
 import kotlinx.coroutines.flow.Flow
 
 /**
- * Interface for UseCases that returns an instance of a [Single].
+ * Interface for UseCases that returns an instance of a [Flow].
  */
-interface SingleUseCase<P, R : Any> {
+interface FlowUseCase<P, R : Any> {
     operator fun invoke(params: P? = null): Flow<R>
 }
